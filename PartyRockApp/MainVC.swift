87873972cf1177926697d8409008aeb7963e03dd
fiachra2690 <coynefiachra@gmail.com>
2017-01-23 23:36:01 +0000

    UI Set Up

---
+++ PartyRockApp/MainVC.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  PartyRockApp
+//
+//  Created by Fiachra Coyne on 23/01/2017.
+//  Copyright © 2017 Fiachra Coyne. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+   
+    }
+
+
+
+
+}
+
